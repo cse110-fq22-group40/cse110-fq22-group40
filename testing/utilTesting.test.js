8735756622d9c0b47be67301e230c2ce734e4949
bsnow1400 <63069372+bsnow1400@users.noreply.github.com>
2022-11-14 19:07:09 -0800

    Add files via upload

---
+++ testing/utilTesting.test.js
@@ -0,0 +1,16 @@
+//import * as utils from "../source/local/utils.js"
+const utils = require("../source/local/Utils.js");
+
+beforeAll(() => {
+    return utils.add_typeF("testProject");
+    return utils.add_typeA("testProject", "testAudio");
+    return utils.add_audio("testProject", "testAudio", "testObject");
+});
+
+afterAll(() => {
+    return utils.remove_typeF_from_local_storage("testProject");
+});
+
+test('Delete type A folder named testAudio', () => {
+    expect(utils.delete_typeA("testProject", "testAudio")).toBe(null);
+});
