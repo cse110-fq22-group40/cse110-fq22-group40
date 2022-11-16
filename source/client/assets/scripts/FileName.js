3c3b19b3bce6cb5ecb57fa1e43877189b1b9acf9
SailorForSchool <114432192+SailorForSchool@users.noreply.github.com>
2022-11-15 22:04:16 -0800

    changes to create button

--- source/client/assets/scripts/FileName.js
+++ source/client/assets/scripts/FileName.js
@@ -7,7 +7,7 @@ class FileName extends HTMLElement {
     filename.className = "fileNameContainer";
 
     let filetitle = document.createElement('h2');
-    filetitle.innerText = "Create Audio Object";
+    filetitle.innerText = "Upload Audio";
     filetitle.className = "fileTitle";
 
     let fileform = document.createElement('form');
@@ -21,8 +21,8 @@ class FileName extends HTMLElement {
     fileinput.className = "fileUpload";
     fileinput.accept = "audio/*";
 
-    let filesubmit = document.createElement('input');
-    filesubmit.type = "submit";
+    let filesubmit = document.createElement('button');
+    filesubmit.textContent = "Create";
     filesubmit.className = "fileCreate";
 
     filesubmit.addEventListener("click", evt => {
@@ -54,6 +54,10 @@ class FileName extends HTMLElement {
       text-align: center;
       width: 450px;
       height: 250px;
+      border: solid;
+      border-radius: 10px;
+      border-width: 1.5xpx;
+      border-color: rgba(0,128,128,1);
       background: linear-gradient(180deg, #DFE9F3 0%, #FFFFFF 100%);
       top: 50%;
       left: 50%;
@@ -84,8 +88,10 @@ class FileName extends HTMLElement {
     .fileCreate{
       position: absolute;
       background: none;
-      border: none;
-      font-size: 30px;
+      border-radius: 5px;
+      border-color: rgba(0,128,128,1);
+      border-width: 1.5px;
+      font-size: 16px;
       right: 0%;
       bottom: 0%;
       margin-bottom: 5px;
