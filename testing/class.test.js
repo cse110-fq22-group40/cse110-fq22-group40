64fa9b24084424f90e7ef89b47baad5b752167f0
SailorEichhorn <114432192+SailorForSchool@users.noreply.github.com>
2022-11-19 16:34:03 -0800

    adding file path for testing valid paths and the necessary folder for holding testing specific materials

--- testing/class.test.js
+++ testing/class.test.js
@@ -1,6 +1,12 @@
 // importing file to test
 const functions = require('../source/local/classes.js');
 
+// this needs to be changed for testing - TODO: find github absolute path
+const TESTING_ABS_PATH = "/Users/sailoreichhorn/Desktop/UCSD/Q4.1/CSE 110/Group 40/REPO/cse110-fq22-group40/testing/testFiles/";
+
+// this can stay the same... add new comment here though :)
+const TESTING_REL_PATH = "../../testing/testFiles/";
+
 // Test: AudioObject
 
 // Basic Tests: constructor + update_path() + get_path()
@@ -28,6 +34,28 @@ test('Test constructor: invalid file path - NON-STRING TYPE', () => {
         newAudio.get_path();
     }
     expect(getter).toThrow();
+});
+
+test('Test constructor: valid file path - RELATIVE PATH?', () => {
+    const input = TESTING_REL_PATH + "moonlight-sonata.mp3";
+    function getter() {
+        const newAudio = new functions.AudioObject(input);
+        // note: this error is thrown by fs.existsSync() i think ???
+        return newAudio.get_path();
+    }
+    var call_to_func = getter();
+    expect(call_to_func).toBe(TESTING_REL_PATH + "moonlight-sonata.mp3");
+});
+
+test('Test constructor: valid file path - ABSOLUTE PATH', () => {
+    const input = TESTING_ABS_PATH + "moonlight-sonata.mp3";
+    function getter() {
+        const newAudio = new functions.AudioObject(input);
+        // note: this error is thrown by fs.existsSync() i think ???
+        return newAudio.get_path();
+    }
+    var call_to_func = getter();
+    expect(call_to_func).toBe(TESTING_REL_PATH + "moonlight-sonata.mp3");
 });
 
 /** TO-DO: Add test cases to test existing files */
