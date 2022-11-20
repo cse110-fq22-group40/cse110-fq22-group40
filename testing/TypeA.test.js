86e32a2887c6dd86910a39930704ad2061b00693
bsnow1400 <63069372+bsnow1400@users.noreply.github.com>
2022-11-19 19:34:45 -0800

    Create TypeA.test.js

--- testing/TypeA.test.js
+++ testing/TypeA.test.js
@@ -7,6 +7,17 @@
 // importing file to test
 const functions = require('../source/local/classes.js');
 
+// allows for testing absolute path without hard coding
+const {resolve} = require('path');
+
+// this needs to be changed for testing - TODO: find github absolute path DONE
+const TESTING_ABS_PATH = resolve('./testFiles');
+
+// this can stay the same... add new comment here though :)
+const TESTING_REL_PATH = "../../testing/testFiles";
+
+
+
 // Basic Tests: constructor, adders, delete
 test('Test constructor typeA:', () => {
     const newTypeA = new functions.TypeA("test_typeA"); 
