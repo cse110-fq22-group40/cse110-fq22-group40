c6c940ee77df4bfae2f99b99e6c76441053f5d5e
Hong <hojin@ucsd.edu>
2022-11-26 13:24:54 -0800

    Fixed a logic error

--- source/client/assets/scripts/type-a/audio-object-create-screen.js
+++ source/client/assets/scripts/type-a/audio-object-create-screen.js
@@ -43,8 +43,8 @@ class AudioObjectCreateScreen extends HTMLElement {
       if (textinput.value.length > 40){
         window.alert("Invalid name. Please use 40 or less characters");
       }
-	  
-      if (fileinput.value == ""){
+
+      else if (fileinput.value == ""){
         window.alert("No file attached. Please choose your audio file");
       }
 
