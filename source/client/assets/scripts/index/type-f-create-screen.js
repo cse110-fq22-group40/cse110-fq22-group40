d4b8ce94b24b8bb9e94510b08845c5a9d5a4bf08
Billy Sudirdja <bsudirdja@ucsd.edu>
2022-11-27 19:52:01 -0800

    fixed styling

--- source/client/assets/scripts/index/type-f-create-screen.js
+++ source/client/assets/scripts/index/type-f-create-screen.js
@@ -8,12 +8,10 @@ const template = document.getElementById("create-screen-template");
 class TypeFCreateScreen extends HTMLElement {
   constructor() {
     super();
-
     const shadow = this.attachShadow({ mode:"open" });
     shadow.appendChild(template.content.cloneNode("true"));
-
     const fileexit = this.shadowRoot.querySelector(".exit");
-  
+
     /**
      * When the exit button is clicked for creating a TypeFCard, close the
      * popup
@@ -24,11 +22,10 @@ class TypeFCreateScreen extends HTMLElement {
     fileexit.addEventListener("click", () => {
       const popup = this.shadowRoot.querySelector(".container");
       popup.remove();
-    });
-  
+    } );
     const filesubmit = this.shadowRoot.querySelector(".create");
     const textinput = this.shadowRoot.querySelector(".name");
-  
+
     /**
      * When the submit button is clicked, create a new eventlistener that can
      * keeps track of the name of the TypeFCard
@@ -38,22 +35,18 @@ class TypeFCreateScreen extends HTMLElement {
      */
     filesubmit.addEventListener("click", evt => {
       evt.preventDefault();
-  
       if (textinput.value.length > 40) {
         window.alert("Invalid name. Please use 40 characters or fewer.");
       }
-      
       else {
         const submitEvent = new CustomEvent("fileSubmitted", {
           detail: {
             name: textinput.value,
           }
-        });
-    
+        } );
         this.dispatchEvent(submitEvent);
       }
-    });
+    } );
   }
 }
-
 customElements.define("type-f-create-screen", TypeFCreateScreen);
