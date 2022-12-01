d74ea758fda01f9f0e300954734ea2bd8af8ee29
otterTheFirst <50503076+hongyuejin@users.noreply.github.com>
2022-11-30 17:59:29 -0800

    Corrected the styling based on the guideline (#168)

--- source/local/app.js
+++ source/local/app.js
@@ -1,3 +1,7 @@
+/**
+ * This file is the entry point of the program and manages the basic lifecycle
+ * of the application.
+ */
 const { app, BrowserWindow, session } = require("electron");
 const path = require("path");
 
@@ -14,7 +18,8 @@ const loadMainWindow = () => {
   mainWindow.show();
   mainWindow.loadFile(path.join(__dirname, "../client/index.html"));
 
-  // TypeA Important for security reasons to protect against cross-site-scripting
+  //TypeA Important for security reasons to protect against 
+  //cross-site-scripting
   session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
     callback({
       responseHeaders: {
@@ -25,14 +30,18 @@ const loadMainWindow = () => {
   });
 }
 
+// Load the main window after Electron has finished initializing
 app.on("ready", loadMainWindow);
 
+// Quit when all windows are closed
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
+// Reload the main window when activating the application with no
+// other windows open.
 app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     loadMainWindow();
