b355abab188eea4b248b7f69f981ef6795ce713c
Emmanuel-Serrano <Eman24.serrano@gmail.com>
2022-12-02 12:40:18 -0800

    Updated/Added Styling

--- source/client/assets/scripts/type-a/audio-object-card.js
+++ source/client/assets/scripts/type-a/audio-object-card.js
@@ -5,19 +5,29 @@
 
 const template = document.getElementById("card-template");
 
+/**
+ * Class for our AudioObjectCard functions and constructor. Total of just two 
+ * functions, one being th constructor and the other being one to set the name.
+ */
 class AudioObjectCard extends HTMLElement {
+  /**
+   * Constructor for AudioObjectCard 
+   * 
+   * @constructor
+   */
   constructor() {
     super();
-    
+
     const shadow = this.attachShadow({ mode: "open" });
     shadow.appendChild(template.content.cloneNode("true"));
 
     /**
-     * When the AudioObjectCard object is clicked, set the path in sessionStorage
-     * and go to the AudioObject.html page
+     * When the AudioObjectCard object is clicked, set the path in 
+     * sessionStorage and go to the AudioObject.html page
      *
      * @type {shadowRoot} - the target of the event
-     * @listens shadowRoot#click - when the AudioObjectCard component is clicked
+     * @listens shadowRoot#click - when the AudioObjectCard component is 
+     * clicked
      */
     this.shadowRoot.addEventListener("click", () => {
       const card = this.shadowRoot.querySelector(".card");
@@ -28,13 +38,13 @@ class AudioObjectCard extends HTMLElement {
         sessionStorage.setItem("AudioObject", audObjectName);
         window.location = "audio-object.html"
       }, 1000);
-    })
+    });
   }
 
   /**
-   * Sets the name of the AudioObjectCard
-   * 
-   * @param {string} name
+   * Sets the name of the AudioObjectCard.
+   *
+   * @param {string} name - the name we want to give the audio card
    */
   set name(name) {
     const audioname = this.shadowRoot.querySelector("h2");
@@ -42,5 +52,5 @@ class AudioObjectCard extends HTMLElement {
   }
 }
 
-// define a custom HTML element
+// defining a custom HTML element
 customElements.define("audio-object-card", AudioObjectCard);
