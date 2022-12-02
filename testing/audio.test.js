be2317c63ca452b7c406e5e7af6cdf488daa863a
Bennett Zhang <20321329+bennett-zhang@users.noreply.github.com>
2022-12-01 20:08:32 -0800

    Fixed styling in audio.test.js

--- testing/audio.test.js
+++ testing/audio.test.js
@@ -4,12 +4,12 @@
  * Billy
  */
   
-// importing file to test
+// Importing file to test
 const aud_fun = require("../source/local/classes/audio-object.js");
 const f_fun = require("../source/local/classes/type-f.js");
 const a_fun = require("../source/local/classes/type-a.js");
 const aud = require("../source/local/scripts/audio.js");
-const {resolve} = require("path");
+const { resolve } = require("path");
 const { hasUncaughtExceptionCaptureCallback } = require("process");
 
 // Constants
@@ -26,23 +26,23 @@ const TEST_REL_PATH_EX2 = TESTING_REL_PATH + NON_EXISTENT_MP3;
 const TEST_ABS_PATH_EX2 = TESTING_ABS_PATH + NON_EXISTENT_MP3;
 
 // test("Testing getting names", () => {
-//     const newTypeF = new f_fun.TypeF("test_typeF"); 
-//     const newTypeA = new a_fun.TypeA("test_typeA");
-//     f_fun.add_typeF("test_typeF");
-//     newTypeF.add_typeA("test_typeA");
-//     newTypeA.add_audio("test_aud", TESTFILE);
-//     console.log(aud.get_all_audio_names("test_typeF", "test_typeA"));
-//   });
+//   const newTypeF = new f_fun.TypeF("test_typeF"); 
+//   const newTypeA = new a_fun.TypeA("test_typeA");
+//   f_fun.add_typeF("test_typeF");
+//   newTypeF.add_typeA("test_typeA");
+//   newTypeA.add_audio("test_aud", TESTFILE);
+//   console.log(aud.get_all_audio_names("test_typeF", "test_typeA"));
+// });
 
-test("Test add_audio: invalid name - EMPTY STRING", () =>{
-    function getter(){
-        const newTypeF = new f_fun.TypeF("test_typeF"); 
-        const newTypeA = new a_fun.TypeA("test_typeA");
-        const newAudio = new aud_fun.AudioObject(TESTFILE);
-        f_fun.add_typeF("test_typeF");
-        newTypeF.add_typeA("test_typeA");
-        newTypeA.add_audio("test_aud", TESTFILE);
-        aud.add_audio("test_typeF", "test_typeA", "", TESTFILE);
-    }
-    expect(getter).toThrow("AudioObj name cannot be empty");
+test("Test add_audio: invalid name - EMPTY STRING", () => {
+  function getter(){
+    const newTypeF = new f_fun.TypeF("test_typeF"); 
+    const newTypeA = new a_fun.TypeA("test_typeA");
+    const newAudio = new aud_fun.AudioObject(TESTFILE);
+    f_fun.add_typeF("test_typeF");
+    newTypeF.add_typeA("test_typeA");
+    newTypeA.add_audio("test_aud", TESTFILE);
+    aud.add_audio("test_typeF", "test_typeA", "", TESTFILE);
+  }
+  expect(getter).toThrow("AudioObj name cannot be empty");
 });
