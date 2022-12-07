8aaecc34f638edbe2ed26929de1203d63df06593
Katherine Wong <katmwong29@gmail.com>
2022-12-07 01:57:22 -0800

    added 2 e2e tests

--- e2e-tests/homepage.test.js
+++ e2e-tests/homepage.test.js
@@ -1,28 +1,57 @@
 const { _electron: electron } = require("playwright")
 const { test, expect } = require("@playwright/test")
 
-test("EXAMPLE", async () => {
-  const electronApp = await electron.launch({ args: ["source/local/app.js"] })
-  const window = await electronApp.firstWindow()
-  await window.screenshot({ path: "e2e-tests/example.png" })
-  // close app
-  await electronApp.close()
-})
+let window;
+let electronApp;
+
+test.beforeAll(async () => {
+  electronApp = await electron.launch({ args: ["source/local/app.js"] })
+  window = await electronApp.firstWindow();
+});
 
 test("Homepage has Team LitHub in the title", async () => {
-  const electronApp = await electron.launch({ args: ["source/local/app.js"] })
-  const window = await electronApp.firstWindow();
+  await window.screenshot({ path: "e2e-tests/example.png" });
+  const title = await window.innerText("title");
+  expect(title).toBe("LitHub Front Page");
+});
+
+test("Create a new project button and deleting a project", async () => {
+  // click on "Create a New Project" button
   await window.locator(".add-button").click();
-  // close app
-  await electronApp.close()
-})
+  // take screenshot
+  await window.screenshot({ path: "e2e-tests/add.png" });
+  // type in input text box to name project
+  const message = window.locator("input.name");
+  // type Classical
+  await message.type("Classical");
+  // clicking on the submit button
+  const submit = window.locator("input.create");
+  await submit.click();
+  // getting the text of the new project
+  const text = await window.locator("h2.card-title").textContent();
+  await expect(text).toBe("Classical");
+  // checking the count of children
+  let count = await window.locator("h2.card-title").count();
+  expect(count).toBe(1);
+  // getting rid of the new project for new tests
+  await window.locator("button#delete-button").click();
+  await window.locator("button.update-yes").click();
+  // checking the count of updated children
+  count = await window.locator("h2.card-title").count();
+  expect(count).toBe(0);
+});
 
 
-test("testing to click on add button", async () => {
-  const electronApp = await electron.launch({ args: ["source/local/app.js"] })
-  const window = await electronApp.firstWindow()
-  await window.screenshot({ path: "e2e-tests/intro.png" })
-  await window.locator()
-  // close app
-  await electronApp.close()
-})
+// test("testing adding type A in type F folder", async () => {
+//   //electronApp = await electron.launch({ args: ["source/local/app.js"] })
+//   //window = await electronApp.firstWindow();
+//   await window.locator(".add-button").click();
+//   await window.screenshot({ path: "e2e-tests/add.png" });
+//   const message = window.locator("input.name");
+//   console.log(await message.inputValue());
+//   await message.type("Classical");
+// });
+
+test.afterAll(async () => {
+  await electronApp.close();
+});
